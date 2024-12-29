a3ae8f152fd8d96739969608a11ccca3019a89d1
Phantom <70034285+phantom2152@users.noreply.github.com>
2024-12-29 17:00:23 +0000

    sample

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./components/Home"
+import About from "./components/About"
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
